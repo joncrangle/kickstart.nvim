d1cb7b60fb720adc35ab044c230eed026e9f4982
Jonathan Crangle <joncrangle@gmail.com>
2024-03-11 23:26:37 -0400

    configure keys for trouble

--- lua/custom/plugins/trouble.lua
+++ lua/custom/plugins/trouble.lua
@@ -1,4 +1,13 @@
 return {
   'folke/trouble.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
+  keys = {
+    {
+      '<leader>x',
+      function()
+        require('trouble').toggle()
+      end,
+      desc = 'Toggle Trouble',
+    },
+  },
 }
