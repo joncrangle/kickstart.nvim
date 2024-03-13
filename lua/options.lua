b10845583efa8f8c08c84b6c509d5e4783f5c32e
Jonathan Crangle <joncrangle@gmail.com>
2024-03-13 01:02:16 -0400

    line and tab options

--- lua/options.lua
+++ lua/options.lua
@@ -7,7 +7,7 @@
 vim.opt.number = true
 -- You can also add relative line numbers, for help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -56,4 +56,14 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- Tab spacing
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+-- Don't highlight search results, but highlight incremental search
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
 -- vim: ts=2 sts=2 sw=2 et
