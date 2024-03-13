91a1eecf2b2b55a9fd8e8982d14b0e5db1bda829
Jonathan Crangle <joncrangle@gmail.com>
2024-03-13 01:02:03 -0400

    config file browser

--- lua/custom/plugins/telescope-file-browser.lua
+++ lua/custom/plugins/telescope-file-browser.lua
@@ -1,6 +1,23 @@
 return {
   'nvim-telescope/telescope-file-browser.nvim',
   dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' },
+  opts = {
+    extensions = {
+      file_browser = {
+        display_stat = { date = true, size = false },
+        sorting_strategy = 'ascending',
+        layout_config = {
+          horizontal = {
+            prompt_position = 'top',
+            preview_width = 0.4,
+          },
+        },
+      },
+    },
+  },
+  config = function(_, opts)
+    require('telescope').setup(opts)
+  end,
   keys = {
     { '<C-f>', '<cmd>Telescope file_browser<cr>', desc = '[F]ile explorer' },
   },
