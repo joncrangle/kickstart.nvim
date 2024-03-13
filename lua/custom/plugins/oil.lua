c496a5b9509f8326b00a12d0e6b1729aca63e2cd
Jonathan Crangle <joncrangle@gmail.com>
2024-03-13 10:33:59 -0400

    add oil.nvim

---
+++ lua/custom/plugins/oil.lua
@@ -0,0 +1,9 @@
+return {
+  'stevearc/oil.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  keys = {
+    '<leader>-',
+    '<cmd>Oil<cr>',
+    { desc = 'Open parent directory in oil.nvim' },
+  },
+}
