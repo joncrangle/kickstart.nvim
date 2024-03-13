401694474831f319cd132dc9b426435796ac3c72
Jonathan Crangle <joncrangle@gmail.com>
2024-03-13 00:11:17 -0400

    remove redundant maps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -49,12 +49,6 @@ vim.keymap.set('x', '<leader>p', '"_dP')
 vim.keymap.set('n', '<leader>d', '"_d')
 vim.keymap.set('v', '<leader>d', '"_d')
 
--- Yank into global register
-vim.keymap.set('n', '<leader>y', '"+y')
-vim.keymap.set('n', '<leader>Y', '"+Y')
-vim.keymap.set('v', '<leader>y', '"+y')
-vim.keymap.set('v', '<leader>Y', '"+Y')
-
 -- In visual mode, move highlighted text up or down
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
