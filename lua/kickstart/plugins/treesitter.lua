ec644d07cc28e711ad6bfd2e570ae7d53d6a03d9
joncrangle <94425204+joncrangle@users.noreply.github.com>
2024-03-19 02:09:00 -0400

    feat: add treesitter-context

--- lua/kickstart/plugins/treesitter.lua
+++ lua/kickstart/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    dependencies = { 'nvim-treesitter/nvim-treesitter-context' },
     build = ':TSUpdate',
     opts = {
       ensure_installed = { 'bash', 'c', 'go', 'html', 'javascript', 'lua', 'markdown', 'typescript', 'vim', 'vimdoc', 'yaml' },
