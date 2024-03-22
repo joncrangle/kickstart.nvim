1511db43bd61e482839c5a4510538e15d28c5bfa
joncrangle <94425204+joncrangle@users.noreply.github.com>
2024-03-21 22:32:32 -0400

    chore: add desc and tab maps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -33,24 +33,29 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Switching and closing tab pages
+vim.keymap.set('n', '<C-j>', '<cmd>tabprevious<CR>', { desc = 'Previous tab page' })
+vim.keymap.set('n', '<C-k>', '<cmd>tabnext<CR>', { desc = 'Next tab page' })
+vim.keymap.set('n', '<C-q>', '<cmd>tabclose<CR>', { desc = 'Close tab page' })
+
 -- Replace current word
 vim.keymap.set('n', '<leader>rw', ':%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>', { desc = '[R]eplace current [w]ord' })
 
 -- Keep cursor in place when joining lines, moving or searching
-vim.keymap.set('n', 'J', 'mzJ`z')
-vim.keymap.set('n', '<C-d>', '<C-d>zz')
-vim.keymap.set('n', '<C-u>', '<C-u>zz')
-vim.keymap.set('n', 'n', 'nzzzv')
-vim.keymap.set('n', 'N', 'Nzzzv')
+vim.keymap.set('n', 'J', 'mzJ`z', { desc = 'Join lines' })
+vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Page down' })
+vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Page up' })
+vim.keymap.set('n', 'n', 'nzzzv', { desc = 'Next search result' })
+vim.keymap.set('n', 'N', 'Nzzzv', { desc = 'Previous search result' })
 
 -- Paste and delete without yanking
-vim.keymap.set('x', '<leader>p', '"_dP')
-vim.keymap.set('n', '<leader>d', '"_d')
-vim.keymap.set('v', '<leader>d', '"_d')
+vim.keymap.set('x', '<leader>p', '"_dP', { desc = 'Paste without yanking' })
+vim.keymap.set('n', '<leader>d', '"_d', { desc = 'Delete without yanking' })
+vim.keymap.set('v', '<leader>d', '"_d', { desc = 'Delete without yanking' })
 
 -- In visual mode, move highlighted text up or down
-vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
-vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move highlighted text up' })
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'Move highlighted text down' })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
