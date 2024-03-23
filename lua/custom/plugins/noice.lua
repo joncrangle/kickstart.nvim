6b6d48902399228fd33678f78dbe5b78c61cfb57
joncrangle <94425204+joncrangle@users.noreply.github.com>
2024-03-22 21:48:26 -0400

    config noice

--- lua/custom/plugins/noice.lua
+++ lua/custom/plugins/noice.lua
@@ -1,13 +1,46 @@
 return {
   {
     'folke/noice.nvim',
-    opts = {},
+    event = 'VeryLazy',
     dependencies = {
       'MunifTanjim/nui.nvim',
-      -- OPTIONAL:
-      --   `nvim-notify` is only needed, if you want to use the notification view.
-      --   If not available, we use `mini` as the fallback
       'rcarriga/nvim-notify',
+    },
+    opts = {
+      lsp = {
+        override = {
+          ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+          ['vim.lsp.util.stylize_markdown'] = true,
+          ['cmp.entry.get_documentation'] = true,
+        },
+      },
+      presets = {
+        bottom_search = true,
+        command_palette = true,
+      },
+    },
+    keys = {
+      {
+        '<leader>nl',
+        function()
+          require('noice').cmd 'last'
+        end,
+        desc = 'Last',
+      },
+      {
+        '<leader>nh',
+        function()
+          require('noice').cmd 'history'
+        end,
+        desc = 'History',
+      },
+      {
+        '<leader>nd',
+        function()
+          require('noice').cmd 'dismiss'
+        end,
+        desc = 'Dismiss all',
+      },
     },
   },
 }
