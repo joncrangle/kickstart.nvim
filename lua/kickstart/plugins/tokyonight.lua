22579ad1f6af69d24be3057959c292e30c9b572a
joncrangle <94425204+joncrangle@users.noreply.github.com>
2024-03-24 00:42:41 -0400

    transparent bg

--- lua/kickstart/plugins/tokyonight.lua
+++ lua/kickstart/plugins/tokyonight.lua
@@ -1,8 +1,5 @@
 return {
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
+  {
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'folke/tokyonight.nvim',
     priority = 500, -- Make sure to load this before all the other start plugins.
@@ -15,6 +12,13 @@ return {
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
     end,
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = 'transparent',
+        floats = 'transparent',
+      },
+    },
   },
 }
 -- vim: ts=2 sts=2 sw=2 et
