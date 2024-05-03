21a971a688228fab5714a7c34d49cc5f016c0a43
jonathancrangle <94425204+joncrangle@users.noreply.github.com>
2024-05-03 09:46:19 -0400

    which-key label for toggle

--- lua/kickstart/plugins/which-key.lua
+++ lua/kickstart/plugins/which-key.lua
@@ -29,6 +29,7 @@ return {
 				["<leader>n"] = { name = "[N]oice", _ = "which_key_ignore" },
 				["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
 				["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
+				["<leader>t"] = { name = "[T]oggle", _ = "which_key_ignore" },
 				["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
 			})
 			-- Ignore Harpoon2 list items
