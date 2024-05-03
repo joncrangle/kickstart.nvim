64a282fa722fcc6d2e22b39c2db51b0d42d38655
jonathancrangle <94425204+joncrangle@users.noreply.github.com>
2024-05-03 11:06:50 -0400

    add biome

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -209,6 +209,7 @@ return {
 			-- for you, so that they are available from within Neovim.
 			local ensure_installed = vim.tbl_keys(servers or {})
 			vim.list_extend(ensure_installed, {
+				"biome",
 				"black",
 				"goimports",
 				"goimports-reviser",
