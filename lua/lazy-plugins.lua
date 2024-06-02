113ab67eb2d5107e53a2351efe573b6614594ec9
jonathancrangle <94425204+joncrangle@users.noreply.github.com>
2024-06-02 00:05:47 -0400

    neovim 0.10 updates

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -21,10 +21,7 @@ require('lazy').setup({
   --
   --  This is equivalent to:
   --    require('Comment').setup({})
-
-  -- "gc" to comment visual regions/lines
-  { 'numToStr/Comment.nvim', opts = {} },
-
+  --
   -- modular approach: using `require 'path/name'` will
   -- include a plugin definition from file lua/path/name.lua
 
