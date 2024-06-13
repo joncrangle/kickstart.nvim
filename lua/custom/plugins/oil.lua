17014657198e2e4d752cccd24b1dd8f0dc621cb7
jonathancrangle <94425204+joncrangle@users.noreply.github.com>
2024-06-12 21:33:02 -0400

    update

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -4,12 +4,17 @@ return {
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   opts = {
     default_file_explorer = true,
+    skip_confirm_for_simple_edits = true,
     view_options = {
       show_hidden = true,
+      natural_order = true,
       ---@diagnostic disable-next-line: unused-local
       is_always_hidden = function(name, bufnr)
         return name == '.git'
       end,
+    },
+    win_options = {
+      wrap = true;
     },
   },
   keys = {
