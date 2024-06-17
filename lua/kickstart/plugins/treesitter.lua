abef76a1b385d69b06fb92498786980c4d96ec66
jonathancrangle <94425204+joncrangle@users.noreply.github.com>
2024-06-17 10:09:06 -0400

    update deprecated func

--- lua/kickstart/plugins/treesitter.lua
+++ lua/kickstart/plugins/treesitter.lua
@@ -106,10 +106,10 @@ return {
       local ts_repeat_move = require 'nvim-treesitter.textobjects.repeatable_move'
       vim.keymap.set({ 'n', 'x', 'o' }, ';', ts_repeat_move.repeat_last_move, { desc = 'Repeat last move' })
       vim.keymap.set({ 'n', 'x', 'o' }, ',', ts_repeat_move.repeat_last_move_opposite, { desc = 'Repeat last move (opposite)' })
-      vim.keymap.set({ 'n', 'x', 'o' }, 'f', ts_repeat_move.builtin_f, { desc = 'Repeat builtin f' })
-      vim.keymap.set({ 'n', 'x', 'o' }, 'F', ts_repeat_move.builtin_F, { desc = 'Repeat builtin F' })
-      vim.keymap.set({ 'n', 'x', 'o' }, 't', ts_repeat_move.builtin_t, { desc = 'Repeat builtin t' })
-      vim.keymap.set({ 'n', 'x', 'o' }, 'T', ts_repeat_move.builtin_T, { desc = 'Repeat builtin T' })
+      vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f_expr, { expr = true })
+      vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F_expr, { expr = true })
+      vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t_expr, { expr = true })
+      vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T_expr, { expr = true })
     end,
   },
   {
