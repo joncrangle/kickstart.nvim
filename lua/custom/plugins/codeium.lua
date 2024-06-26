02ead2a38c019eda30c980e6ed17ddf7f9308346
jonathancrangle <94425204+joncrangle@users.noreply.github.com>
2024-06-25 21:57:24 -0400

    codeium fixes

--- lua/custom/plugins/codeium.lua
+++ lua/custom/plugins/codeium.lua
@@ -3,6 +3,16 @@ return {
     'Exafunction/codeium.vim',
     lazy = true,
     event = 'BufEnter',
+    config = function()
+      vim.g.codeium_disable_bindings = true
+      vim.g.codeium_enabled = true
+      vim.keymap.set('i', '<Tab>', function () return vim.fn['codeium#Accept']() end, { expr = true, silent = true, desc = "Codeium Accept" })
+      vim.keymap.set('i', '<C-a>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true, silent = true, desc = "Codeium Next Completion" })
+      vim.keymap.set('i', '<C-z>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true, silent = true, desc = "Codeium Previous Completion" })
+      vim.keymap.set('i', '<C-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true, desc = "Codeium Clear" })
+      vim.keymap.set('i', '<C-\\>', function() return vim.fn['codeium#Complete']() end, { expr = true, silent = true, desc = "Codeium Trigger Completion" })
+      vim.keymap.set('n', '<leader>cc', function() return vim.fn['codeium#Chat']() end, { desc = "  [C]hat with [C]odeium" })
+    end
   },
 }
 -- vim: ts=2 sts=2 sw=2 et
