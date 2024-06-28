b29b81160e5b5dbed946021c4807b4563e4abc21
jonathancrangle <94425204+joncrangle@users.noreply.github.com>
2024-06-28 10:20:39 -0400

    different markdown previewer

--- lua/custom/plugins/markdown.lua
+++ lua/custom/plugins/markdown.lua
@@ -1,14 +1,9 @@
 return {
-  'MeanderingProgrammer/markdown.nvim',
-  dependencies = { 'nvim-treesitter/nvim-treesitter' },
+  "OXY2DEV/markview.nvim",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
   ft = { 'markdown' },
   opts = true,
-  keys = {
-    {
-      '<leader>tm',
-      '<cmd>RenderMarkdownToggle<cr>',
-      desc = '[T]oggle [m]arkdown preview',
-    },
-  },
 }
 -- vim: ts=2 sts=2 sw=2 et
