e7e96ef0d62827d117177a876059435888b3ddb6
jonathancrangle <94425204+joncrangle@users.noreply.github.com>
2024-07-06 21:23:21 -0400

    add wezterm config file search shortcut

--- lua/kickstart/plugins/telescope.lua
+++ lua/kickstart/plugins/telescope.lua
@@ -124,6 +124,11 @@ return {
       vim.keymap.set('n', '<leader>sn', function()
         builtin.find_files { cwd = vim.fn.stdpath 'config' }
       end, { desc = '[S]earch [N]eovim files' })
+
+      -- Shortcut for searching Wezterm configuration files
+      vim.keymap.set('n', '<leader>st', function()
+        builtin.find_files { cwd = '$XDG_CONFIG_HOME/wezterm' }
+      end, { desc = '[S]earch Wez[t]erm config files' })
     end,
   },
 }
