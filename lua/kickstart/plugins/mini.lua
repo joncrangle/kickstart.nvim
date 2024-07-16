5558d269bb8064194715ab43d6755088121faaa6
jonathancrangle <94425204+joncrangle@users.noreply.github.com>
2024-07-16 13:32:00 -0400

    add mini.pairs

--- lua/kickstart/plugins/mini.lua
+++ lua/kickstart/plugins/mini.lua
@@ -19,6 +19,22 @@ return {
       -- - sr)'  - [S]urround [R]eplace [)] [']
       require('mini.surround').setup()
 
+      require('mini.pairs').setup {
+        event = 'VeryLazy',
+        opts = {
+          modes = { insert = true, command = true, terminal = false },
+          -- skip autopair when next character is one of these
+          skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+          -- skip autopair when the cursor is inside these treesitter nodes
+          skip_ts = { 'string' },
+          -- skip autopair when next character is closing pair
+          -- and there are more closing pairs than opening pairs
+          skip_unbalanced = true,
+          -- better deal with markdown code blocks
+          markdown = true,
+        },
+      }
+
       -- Icon provider
       require('mini.icons').setup()
       -- Until other plugins support mini.icons natively
