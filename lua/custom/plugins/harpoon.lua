63a07b12989b1198d687c857f7a8410a0211a7d5
jonathancrangle <94425204+joncrangle@users.noreply.github.com>
2024-07-16 13:31:45 -0400

    simplify

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -3,44 +3,17 @@ return {
     'theprimeagen/harpoon',
     branch = 'harpoon2',
     lazy = true,
-    event = 'VeryLazy',
     dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = {
+      menu = {
+        width = vim.api.nvim_win_get_width(0) - 4,
+      },
+      settings = {
+        save_on_toggle = true,
+      },
+    },
     config = function()
       local harpoon = require 'harpoon'
-      harpoon:setup {}
-
-      -- basic telescope configuration
-      local conf = require('telescope.config').values
-      local function toggle_telescope(harpoon_files)
-        local file_paths = {}
-        for _, item in ipairs(harpoon_files.items) do
-          table.insert(file_paths, item.value)
-        end
-
-        require('telescope.pickers')
-          .new({}, {
-            prompt_title = 'Harpoon',
-            finder = require('telescope.finders').new_table {
-              results = file_paths,
-            },
-            previewer = conf.file_previewer {},
-            sorter = conf.generic_sorter {},
-          })
-          :find()
-      end
-
-      vim.keymap.set('n', '<C-e>', function()
-        toggle_telescope(harpoon:list())
-      end, { desc = 'Open telescope Harpoon2 window' })
-
-      vim.keymap.set('n', '<leader>h', function()
-        harpoon.ui:toggle_quick_menu(harpoon:list())
-      end, { desc = 'Harpoon2 quick menu' })
-
-      vim.keymap.set('n', '<leader>a', function()
-        harpoon:list():add()
-      end, { desc = '[A]dd file to Harpoon2' })
-
       harpoon:extend {
         UI_CREATE = function(cx)
           vim.keymap.set('n', '<C-v>', function()
@@ -56,12 +29,36 @@ return {
           end, { buffer = cx.bufnr })
         end,
       }
+    end,
+    keys = function()
+      local keys = {
+        {
+          '<leader>h',
+          function()
+            local harpoon = require 'harpoon'
+            harpoon.ui:toggle_quick_menu(harpoon:list())
+          end,
+          desc = '[H]arpoon2 quick menu',
+        },
+        {
+          '<leader>a',
+          function()
+            require('harpoon'):list():add()
+          end,
+          desc = '[A]dd file to Harpoon2',
+        },
+      }
 
       for i = 1, 5 do
-        vim.keymap.set('n', '<leader>' .. i, function()
-          harpoon:list():select(i)
-        end, { desc = 'Harpoon list ' .. i })
+        table.insert(keys, {
+          '<leader>' .. i,
+          function()
+            require('harpoon'):list():select(i)
+          end,
+          desc = 'Harpoon2 list ' .. i,
+        })
       end
+      return keys
     end,
   },
 }
