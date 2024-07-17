4c6e7aed49fa9da014947de2f7576c00bcca7d44
jonathancrangle <94425204+joncrangle@users.noreply.github.com>
2024-07-17 10:31:26 -0400

    swap markdown renderer

--- lua/custom/plugins/markdown.lua
+++ lua/custom/plugins/markdown.lua
@@ -1,16 +1,37 @@
 return {
-  'OXY2DEV/markview.nvim',
-  dependencies = {
-    'echasnovski/mini.nvim',
-    'nvim-treesitter/nvim-treesitter',
+  {
+    'OXY2DEV/markview.nvim',
+    dependencies = {
+      'echasnovski/mini.nvim',
+      'nvim-treesitter/nvim-treesitter',
+    },
+    enabled = false,
+    ft = { 'markdown' },
+    opts = true,
+    keys = {
+      {
+        '<leader>tm',
+        '<cmd>Markview<cr>',
+        ft = 'markdown',
+        desc = '[T]oggle [M]arkview',
+      },
+    },
   },
-  ft = { 'markdown' },
-  opts = true,
-  keys = {
-    {
-      '<leader>tm',
-      '<cmd>Markview<cr>',
-      desc = '[T]oggle [M]arkview',
+  {
+    'MeanderingProgrammer/markdown.nvim',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'echasnovski/mini.nvim',
+    },
+    ft = { 'markdown' },
+    opts = true,
+    keys = {
+      {
+        '<leader>tm',
+        '<cmd>RenderMarkdownToggle<cr>',
+        ft = 'markdown',
+        desc = '[T]oggle [M]arkdown',
+      },
     },
   },
 }
