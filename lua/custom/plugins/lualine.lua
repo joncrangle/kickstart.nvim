49bf39fb1f2c853021d1f64e2108fd9b5efe9378
jonathancrangle <94425204+joncrangle@users.noreply.github.com>
2024-07-22 01:23:54 -0400

    lazy load lualine

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -2,6 +2,17 @@ return {
   {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'echasnovski/mini.nvim' },
+    event = 'VeryLazy',
+    init = function()
+      vim.g.lualine_laststatus = vim.o.laststatus
+      if vim.fn.argc(-1) > 0 then
+        -- set an empty statusline till lualine loads
+        vim.o.statusline = ' '
+      else
+        -- hide the statusline on the starter page
+        vim.o.laststatus = 0
+      end
+    end,
     opts = {
       options = {
         theme = 'catppuccin',
@@ -14,6 +25,7 @@ return {
           'quickfix',
           'trouble',
         },
+        disabled_filetypes = { statusline = { 'dashboard' } },
       },
     },
   },
