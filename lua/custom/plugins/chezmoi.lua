a324c5f61d734c96bb56e36fe636535f75a2b042
jonathancrangle <94425204+joncrangle@users.noreply.github.com>
2024-07-26 14:32:19 -0400

    add chezmoi.vim

--- lua/custom/plugins/chezmoi.lua
+++ lua/custom/plugins/chezmoi.lua
@@ -4,8 +4,20 @@ end
 
 return {
   {
+    -- highlighting for chezmoi files template files
+    'alker0/chezmoi.vim',
+    event = { 'BufRead', 'BufNewFile' },
+    init = function()
+      vim.g['chezmoi#use_tmp_buffer'] = 1
+      vim.g['chezmoi#source_dir_path'] = (
+        vim.fn.has 'win32' == 1 and os.getenv 'USERPROFILE' .. '/AppData/Local/chezmoi' or os.getenv 'HOME' .. '/.local/share/chezmoi'
+      )
+    end,
+  },
+  {
     'xvzc/chezmoi.nvim',
     cmd = { 'ChezmoiEdit', 'ChezmoiList' },
+    event = { 'BufRead', 'BufNewFile' },
     keys = {
       { '<leader>sc', pick_chezmoi, desc = '[S]earch [C]hezmoi' },
     },
@@ -13,7 +25,7 @@ return {
       -- run chezmoi edit on file enter
       vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
         pattern = {
-          (vim.fn.has 'win32' == 1 and os.getenv 'USERPROFILE' .. '/AppData/Local/chezmoi/*' or os.getenv 'HOME' .. '/.local/share/chezmoi/*'),
+          vim.fn.has 'win32' == 1 and (os.getenv 'USERPROFILE' .. '/AppData/Local/chezmoi/*') or (os.getenv 'HOME' .. '/.local/share/chezmoi/*'),
         },
         callback = function()
           vim.schedule(require('chezmoi.commands.__edit').watch)
