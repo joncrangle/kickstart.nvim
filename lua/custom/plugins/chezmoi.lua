4506cc1b70164615f32021db37cb6ee75796b7cc
jonathancrangle <94425204+joncrangle@users.noreply.github.com>
2024-07-26 15:26:21 -0400

    disable chezmoi autocmd

--- lua/custom/plugins/chezmoi.lua
+++ lua/custom/plugins/chezmoi.lua
@@ -17,21 +17,21 @@ return {
   {
     'xvzc/chezmoi.nvim',
     cmd = { 'ChezmoiEdit', 'ChezmoiList' },
-    event = { 'BufRead', 'BufNewFile' },
+    -- event = { 'BufRead', 'BufNewFile' },
     keys = {
       { '<leader>sc', pick_chezmoi, desc = '[S]earch [C]hezmoi' },
     },
-    init = function()
-      -- run chezmoi edit on file enter
-      vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
-        pattern = {
-          vim.fn.has 'win32' == 1 and (os.getenv 'USERPROFILE' .. '/AppData/Local/chezmoi/*') or (os.getenv 'HOME' .. '/.local/share/chezmoi/*'),
-        },
-        callback = function()
-          vim.schedule(require('chezmoi.commands.__edit').watch)
-        end,
-      })
-    end,
+    -- init = function()
+    --   -- run chezmoi edit on file enter
+    --   vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
+    --     pattern = {
+    --       vim.fn.has 'win32' == 1 and (os.getenv 'USERPROFILE' .. '/AppData/Local/chezmoi/*') or (os.getenv 'HOME' .. '/.local/share/chezmoi/*'),
+    --     },
+    --     callback = function()
+    --       vim.schedule(require('chezmoi.commands.__edit').watch)
+    --     end,
+    --   })
+    -- end,
   },
 }
 -- vim: ts=2 sts=2 sw=2 et
