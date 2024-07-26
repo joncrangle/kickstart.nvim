6789ba54c7ad63125e7ca20630338f7b17bc9531
jonathancrangle <94425204+joncrangle@users.noreply.github.com>
2024-07-26 16:32:34 -0400

    change env to xdg_data_home

--- lua/custom/plugins/chezmoi.lua
+++ lua/custom/plugins/chezmoi.lua
@@ -13,9 +13,7 @@ return {
     -- init = function()
     --   -- run chezmoi edit on file enter
     --   vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
-    --     pattern = {
-    --       vim.fn.has 'win32' == 1 and (os.getenv 'USERPROFILE' .. '/AppData/Local/chezmoi/*') or (os.getenv 'HOME' .. '/.local/share/chezmoi/*'),
-    --     },
+    --     pattern = { os.getenv 'XDG_DATA_HOME' .. '/chezmoi/*' },
     --     callback = function()
     --       vim.schedule(require('chezmoi.commands.__edit').watch)
     --     end,
