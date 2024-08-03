6b411aa2fc809f32b1891b646148064b8cba5c86
jonathancrangle <94425204+joncrangle@users.noreply.github.com>
2024-08-03 18:55:34 -0400

    add conform toggle command

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -12,13 +12,15 @@ return {
         mode = '',
         desc = '[F]ormat buffer',
       },
+      {
+        '<leader>tf',
+        '<cmd>FormatToggle<cr>',
+        mode = 'n',
+        desc = '[T]oggle [F]ormat on save',
+      },
     },
     opts = {
       notify_on_error = false,
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_format = 'fallback',
-      },
       formatters_by_ft = {
         astro = { 'prettierd', 'prettier', stop_after_first = true },
         bash = { 'shfmt' },
@@ -41,6 +43,26 @@ return {
         zsh = { 'shfmt' },
       },
     },
+    config = function()
+      require('conform').setup {
+        format_on_save = function(bufnr)
+          if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+            return
+          end
+          return { timeout_ms = 500, lsp_format = 'fallback' }
+        end,
+      }
+
+      vim.api.nvim_create_user_command('FormatToggle', function()
+        if vim.g.disable_autoformat then
+          vim.g.disable_autoformat = false
+        else
+          vim.g.disable_autoformat = true
+        end
+      end, {
+        desc = 'Toggle autoformat-on-save',
+      })
+    end,
   },
 }
 -- vim: ts=2 sts=2 sw=2 et
