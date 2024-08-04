227b0c8426ba3b551540ed9f620de1c6bb017f45
jonathancrangle <94425204+joncrangle@users.noreply.github.com>
2024-08-03 23:44:39 -0400

    disable hardtime.nvim by default

--- lua/custom/plugins/hardtime.lua
+++ lua/custom/plugins/hardtime.lua
@@ -1,8 +1,11 @@
 return {
   'm4xshen/hardtime.nvim',
   dependencies = { 'MunifTanjim/nui.nvim', 'nvim-lua/plenary.nvim' },
-  event = { 'BufReadPost', 'BufNewFile' },
-  opts = true,
+  cmd = 'Hardtime',
+  -- event = { 'BufReadPost', 'BufNewFile' },
+  opts = {
+    enabled = false,
+  },
   enabled = true,
   keys = {
     {
