b5b98ad84fad129048b159ede1f3ef589fdc0038
jonathancrangle <94425204+joncrangle@users.noreply.github.com>
2024-08-12 23:57:51 -0400

    disable unused colorschemes

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -2,6 +2,7 @@ return {
   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
   {
     'catppuccin/nvim',
+    enabled = true,
     name = 'catppuccin',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
@@ -71,10 +72,11 @@ return {
   },
   {
     "scottmckendry/cyberdream.nvim",
+    enabled = false,
     priority = 1000,
-    -- init = function()
-    --   vim.cmd.colorscheme 'cyberdream'
-    -- end,
+    init = function()
+      vim.cmd.colorscheme 'cyberdream'
+    end,
     opts = {
       transparent = true,
     },
@@ -82,9 +84,9 @@ return {
   {
     'rebelot/kanagawa.nvim',
     priority = 1000,
-    -- init = function()
-    --   vim.cmd.colorscheme 'kanagawa-wave'
-    -- end,
+    init = function()
+      vim.cmd.colorscheme 'kanagawa-wave'
+    end,
     opts = {
       transparent = true,
       overrides = function(colors)
@@ -93,12 +95,10 @@ return {
           NormalFloat = { bg = "none" },
           FloatBorder = { bg = "none" },
           FloatTitle = { bg = "none" },
-
           -- Save an hlgroup with dark background and dimmed foreground
           -- so that you can use it where your still want darker windows.
           -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
           NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
-
           -- Popular plugins that open floats will link to NormalFloat by default;
           -- set their background accordingly if you wish to keep them dark and borderless
           LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
@@ -120,10 +120,11 @@ return {
   },
   {
     'folke/tokyonight.nvim',
+    enabled = false,
     priority = 1000,
-    -- init = function()
-    --   vim.cmd.colorscheme 'tokyonight-night'
-    -- end,
+    init = function()
+      vim.cmd.colorscheme 'tokyonight-night'
+    end,
     opts = {
       transparent = true,
       styles = {
@@ -134,31 +135,34 @@ return {
   },
   {
     'EdenEast/nightfox.nvim',
+    enabled = false,
     priority = 1000,
-    -- init = function()
-    --   vim.cmd.colorscheme 'nightfox'
-    -- end,
+    init = function()
+      vim.cmd.colorscheme 'nightfox'
+    end,
     opts = {
       transparent = false,
     },
   },
   {
     'rose-pine/neovim',
+    enabled = false,
     name = "rose-pine",
     priority = 1000,
-    -- init = function()
-    --   vim.cmd.colorscheme 'rose-pine'
-    -- end,
+    init = function()
+      vim.cmd.colorscheme 'rose-pine'
+    end,
     opts = {
       transparent = true,
     },
   },
   {
     "Mofiqul/dracula.nvim",
+    enabled = false,
     priority = 1000,
-    -- init = function()
-    --   vim.cmd.colorscheme 'dracula'
-    -- end,
+    init = function()
+      vim.cmd.colorscheme 'dracula'
+    end,
     opts = {
       transparent = true,
     },
