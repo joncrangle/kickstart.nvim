f0ed166e61bc7fef442e7e7f3c829d8131d7e3ba
joncrangle <94425204+joncrangle@users.noreply.github.com>
2024-08-21 15:34:10 -0400

    Update markdown.lua

--- lua/custom/plugins/markdown.lua
+++ lua/custom/plugins/markdown.lua
@@ -18,7 +18,7 @@ return {
     },
   },
   {
-    'MeanderingProgrammer/markdown.nvim',
+    'MeanderingProgrammer/render-markdown.nvim',
     dependencies = {
       'nvim-treesitter/nvim-treesitter',
       'echasnovski/mini.nvim',
