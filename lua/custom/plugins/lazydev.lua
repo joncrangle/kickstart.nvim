be6cd45aea4c2e4bef56eb722668e73a45b3920a
jonathancrangle <94425204+joncrangle@users.noreply.github.com>
2024-09-06 16:08:06 -0400

    fix wezterm-types

--- lua/custom/plugins/lazydev.lua
+++ lua/custom/plugins/lazydev.lua
@@ -1,6 +1,7 @@
 -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
 -- used for completion, annotations and signatures of Neovim apis
 return {
+  { 'Bilal2453/luvit-meta',        lazy = true, ft = 'lua' },
   { 'justinsgithub/wezterm-types', lazy = true, ft = 'lua' },
   {
     'folke/lazydev.nvim',
@@ -9,11 +10,10 @@ return {
     opts = {
       library = {
         -- Load luvit types when the `vim.uv` word is found
-        { path = 'luvit-meta/library', words = { 'vim%.uv' } },
-        { path = 'wezterm-types',      mods = { 'wezterm' } },
+        { path = 'luvit-meta/library',  words = { 'vim%.uv' } },
+        { path = 'wezterm-types/types', mods = { 'wezterm' } },
       },
     },
   },
-  { 'Bilal2453/luvit-meta',        lazy = true },
 }
 -- vim: ts=2 sts=2 sw=2 et
