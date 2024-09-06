e2342b8de00090afce981be2baf72805c0eec0c7
jonathancrangle <94425204+joncrangle@users.noreply.github.com>
2024-09-06 13:28:45 -0400

    add markdown previewer

--- lua/custom/plugins/markdown.lua
+++ lua/custom/plugins/markdown.lua
@@ -1,23 +1,5 @@
 return {
   {
-    'OXY2DEV/markview.nvim',
-    dependencies = {
-      'echasnovski/mini.nvim',
-      'nvim-treesitter/nvim-treesitter',
-    },
-    enabled = false,
-    ft = { 'markdown' },
-    opts = {},
-    keys = {
-      {
-        '<leader>tm',
-        '<cmd>Markview<cr>',
-        ft = 'markdown',
-        desc = '[T]oggle [M]arkview',
-      },
-    },
-  },
-  {
     'MeanderingProgrammer/render-markdown.nvim',
     dependencies = {
       'nvim-treesitter/nvim-treesitter',
@@ -32,6 +14,18 @@ return {
         ft = 'markdown',
         desc = '[T]oggle [M]arkdown',
       },
+    },
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = "cd app && npm install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    keys = {
+      { "<leader>tp", "<cmd>MarkdownPreviewToggle<cr>", desc = "[T]oggle Markdown [P]review" },
     },
   },
 }
