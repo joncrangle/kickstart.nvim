374c539e2cb007c4698169cc99df88d22493eccf
jonathancrangle <94425204+joncrangle@users.noreply.github.com>
2024-09-15 14:12:43 -0400

    update lazygit config

--- lua/custom/plugins/lazygit.lua
+++ lua/custom/plugins/lazygit.lua
@@ -2,15 +2,19 @@ return {
   {
     'kdheepak/lazygit.nvim',
     lazy = true,
-    cmd = 'LazyGit',
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
     dependencies = {
-      'nvim-telescope/telescope.nvim',
       'nvim-lua/plenary.nvim',
     },
-    config = function()
-      require('telescope').load_extension 'lazygit'
-    end,
-    vim.keymap.set('n', '<leader>gg', '<cmd>LazyGit<cr>', { desc = 'Open Lazy[G]it' }),
+    keys = {
+      { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+    }
   },
 }
 -- vim: ts=2 sts=2 sw=2 et
