47498017c32c495a500f64ecbdf969150437c20a
jonathancrangle <94425204+joncrangle@users.noreply.github.com>
2024-09-26 12:05:10 -0400

    add telescope mappings

--- lua/kickstart/plugins/telescope.lua
+++ lua/kickstart/plugins/telescope.lua
@@ -55,13 +55,13 @@ return {
       require('telescope').setup {
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
-        --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
         defaults = {
+          mappings = {
+            n = {
+              ['d'] = require('telescope.actions').delete_buffer,
+              ['q'] = require('telescope.actions').close,
+            },
+          },
           file_ignore_patterns = {
             'node_modules',
             'build',
