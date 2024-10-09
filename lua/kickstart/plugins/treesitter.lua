576d2928bf3dbcb3909e85e13ad51724e52edcda
jonathancrangle <94425204+joncrangle@users.noreply.github.com>
2024-10-09 00:55:18 -0400

    remove deprecated ruby highlighting

--- lua/kickstart/plugins/treesitter.lua
+++ lua/kickstart/plugins/treesitter.lua
@@ -43,13 +43,7 @@ return {
       },
       -- Autoinstall languages that are not installed
       auto_install = true,
-      highlight = {
-        enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
-      },
+      highlight = { enable = true },
       indent = { enable = true, disable = { 'ruby' } },
       incremental_selection = {
         enable = true,
