2f64ba1724e1db4c1ceb77c9cadb84627d41c215
jonathancrangle <94425204+joncrangle@users.noreply.github.com>
2024-10-30 15:59:55 -0400

    update blink keymaps

--- lua/kickstart/plugins/cmp.lua
+++ lua/kickstart/plugins/cmp.lua
@@ -175,9 +175,18 @@ return {
         },
       },
       keymap = {
-        accept = '<C-y>',
-        snippet_forward = '<C-l>',
-        snippet_backward = '<C-h>',
+        ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+        ['<C-e>'] = { 'hide' },
+        ['<C-y>'] = { 'select_and_accept' },
+
+        ['<C-p>'] = { 'select_prev', 'fallback' },
+        ['<C-n>'] = { 'select_next', 'fallback' },
+
+        ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+        ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+
+        ['<C-l>'] = { 'snippet_forward', 'fallback' },
+        ['<C-h>'] = { 'snippet_backward', 'fallback' },
       },
     },
   },
