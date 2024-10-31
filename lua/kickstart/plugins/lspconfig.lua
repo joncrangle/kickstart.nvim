efa5d27f42fb301234e6f61a67574d1e7eb08e85
jonathancrangle <94425204+joncrangle@users.noreply.github.com>
2024-10-31 12:34:51 -0400

    extend lsp capabilities with blink

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -12,8 +12,8 @@ return {
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
       { 'j-hui/fidget.nvim',       opts = {} },
 
-      -- Allows extra capabilities provided by nvim-cmp
-      -- { 'hrsh7th/cmp-nvim-lsp',    lazy = true },
+      -- Allows extra capabilities provided by blink.cmp
+      { 'saghen/blink.cmp' },
 
       { 'b0o/schemastore.nvim',    lazy = true,   opts = nil },
     },
@@ -165,9 +165,9 @@ return {
       -- LSP servers and clients are able to communicate to each other what features they support.
       --  By default, Neovim doesn't support everything that is in the LSP specification.
       --  When you add nvim-cmp, nvim-snippets, etc. Neovim now has *more* capabilities.
-      --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
+      --  So, we create new capabilities with blink.cmp, and then broadcast that to the servers.
       local capabilities = vim.lsp.protocol.make_client_capabilities()
-      -- capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+      capabilities = vim.tbl_deep_extend('force', capabilities, require('blink.cmp').get_lsp_capabilities(capabilities))
 
       -- Enable the following language servers
       --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
