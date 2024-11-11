37afbe1ff4e27d107a7b460885e9dbf9331ac76c
jonathancrangle <94425204+joncrangle@users.noreply.github.com>
2024-11-11 18:15:15 -0500

    update llm to qwen2.5 coder 14b

--- lua/custom/plugins/llm.lua
+++ lua/custom/plugins/llm.lua
@@ -32,9 +32,9 @@ return {
     build = vim.fn.has 'win32' == 0 and 'make' or
         'pwsh.exe -NoProfile -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false',
     opts = {
-      provider = 'supernova',
+      provider = 'qwen',
       vendors = {
-        ['qwen'] = generate_vendor('5001', 'qwen2.5-coder-7b-instruct-8bit-q8'),
+        ['qwen'] = generate_vendor('5001', 'qwen2.5-coder-14b-instruct-4bit-q4'),
         ['supernova'] = generate_vendor('5001', 'supernova-medius-4bit-q4'),
         ['llama'] = generate_vendor('5001', 'meta-llama-3.1-8b-instruct-8bit-q8'),
         ['replete'] = generate_vendor('11434', 'Replete-LLM-V2.5-Qwen:14b-q4_K_M'),
