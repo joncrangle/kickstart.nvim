1f90b9db15f037a57e31cf8670c10b971941442a
jonathancrangle <94425204+joncrangle@users.noreply.github.com>
2024-12-02 17:11:52 -0500

    add inline diagnostics

--- lua/custom/plugins/trouble.lua
+++ lua/custom/plugins/trouble.lua
@@ -11,5 +11,13 @@ return {
       desc = 'Toggle Trouble',
     },
   },
+  {
+    'rachartier/tiny-inline-diagnostic.nvim',
+    event = 'VeryLazy',
+    priority = 1000,
+    config = function()
+      require('tiny-inline-diagnostic').setup()
+    end
+  },
 }
 -- vim: ts=2 sts=2 sw=2 et
