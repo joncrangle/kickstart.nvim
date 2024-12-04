2e9aa77029347c8a922acc173a57d64c8f6201ac
joncrangle <94425204+joncrangle@users.noreply.github.com>
2024-12-04 14:21:07 -0500

    fix: trouble and diagnostics

--- lua/custom/plugins/trouble.lua
+++ lua/custom/plugins/trouble.lua
@@ -1,23 +1,26 @@
 return {
-  'folke/trouble.nvim',
-  lazy = true,
-  opts = {},
-  cmd = 'Trouble',
-  dependencies = { 'echasnovski/mini.nvim' },
-  keys = {
-    {
-      '<leader>x',
-      '<cmd>Trouble diagnostics toggle<cr>',
-      desc = 'Toggle Trouble',
+  {
+    'folke/trouble.nvim',
+    lazy = true,
+    opts = {},
+    cmd = 'Trouble',
+    dependencies = { 'echasnovski/mini.nvim' },
+    keys = {
+      {
+        '<leader>x',
+        '<cmd>Trouble diagnostics toggle<cr>',
+        desc = 'Toggle Trouble',
+      },
     },
   },
   {
     'rachartier/tiny-inline-diagnostic.nvim',
-    event = 'VeryLazy',
+    event = 'LspAttach',
     priority = 1000,
     config = function()
+      vim.diagnostic.config({ virtual_text = false })
       require('tiny-inline-diagnostic').setup()
-    end
+    end,
   },
 }
 -- vim: ts=2 sts=2 sw=2 et
